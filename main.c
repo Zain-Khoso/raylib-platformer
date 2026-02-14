5ecbdd1802f80fa41cc560115d9c116cb31f7877
Zain-Khoso <zain.khoso.dev@gmail.com>
2026-02-14 18:53:24 +0500

    new: Terrain setup

--- main.c
+++ main.c
@@ -6,6 +6,10 @@
 #define FPS 60
 #define GRAVITY 1
 
+// Terrain
+#define TILE_SIZE 64
+#define TILE_AMOUNT 1000
+
 // Player
 #define PLAYER_SPEED 12
 #define PLAYER_JUMP_POWER 20
@@ -30,6 +34,53 @@ int main(void)
     // Loading player
     Player player = {0};
     player.velocity = 0;
+
+    // Loading terrain
+    Rectangle tiles[TILE_AMOUNT] = {0};
+
+    char *level_map[] = {
+        "    P                                                                               ",
+        "                                                                                    ",
+        "                                                                                    ",
+        " XX    XXX            XX     XX    XXX            XX     XX    XXX            XX    ",
+        " XX                          XX                          XX                         ",
+        " XXXX         XX         XX  XXXX         XX         XX  XXXX         XX         XX ",
+        " XXXX       XX               XXXX       XX               XXXX       XX              ",
+        " XX    X  XXXX    XX  XX     XX    X  XXXX    XX  XX     XX    X  XXXX    XX  XX    ",
+        "       X  XXXX    XX  XXX          X  XXXX    XX  XXX          X  XXXX    XX  XXX   ",
+        "    XXXX  XXXXXX  XX  XXXX      XXXX  XXXXXX  XX  XXXX      XXXX  XXXXXX  XX  XXXX  ",
+        "XXXXXXXX  XXXXXX  XX  XXXX  XXXXXXXX  XXXXXX  XX  XXXX  XXXXXXXX  XXXXXX  XX  XXXX  ",
+    };
+
+    int level_rows = sizeof(level_map) / sizeof(level_map[0]);
+    int level_cols = strlen(level_map[0]);
+
+    for (int row = 0; row < level_rows; row++)
+    {
+        for (int col = 0; col < level_cols; col++)
+        {
+            int x_pos = col * TILE_SIZE;
+            int y_pos = row * TILE_SIZE;
+
+            if (level_map[row][col] == 'X')
+            {
+                Rectangle tile = {0};
+
+                tile.width = TILE_SIZE;
+                tile.height = TILE_SIZE;
+                tile.x = x_pos;
+                tile.y = y_pos;
+                tiles[(row * level_cols) + col] = tile;
+            }
+
+            else if (level_map[row][col] == 'P')
+            {
+                player.position.x = x_pos;
+                player.position.y = y_pos;
+            }
+        }
+    }
+
     // Redering
     SetTargetFPS(60);
 
@@ -38,6 +89,12 @@ int main(void)
         BeginDrawing();
 
         ClearBackground(DARKGRAY);
+
+        // Terrain
+        for (int i = 0; i < TILE_AMOUNT; i++)
+        {
+            DrawRectangleRec(tiles[i], LIGHTGRAY);
+        }
 
         if (IsKeyPressed(KEY_SPACE))
         {
