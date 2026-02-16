cc358257ea7efb4cb8811ca7ba97a21965feac2b
Zain-Khoso <zain.khoso.dev@gmail.com>
2026-02-16 15:06:08 +0500

    new: Shared header file

---
+++ shared.h
@@ -0,0 +1,14 @@
+#ifndef SHARED_H
+
+#define SHARED_H
+
+#include <raylib.h>
+
+#define TILE_SIZE 64
+#define GRAVITY 1
+#define PLAYER_SPEED 12
+#define PLAYER_JUMP_POWER 20
+
+extern const char *level_map[];
+
+#endif
